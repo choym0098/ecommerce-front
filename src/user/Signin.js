701d3cbf3dac69f33dbd48602bd18404459e7a06
choym0098 <choym0098@gmail.com>
2020-06-24 17:04:05 +0900

    Create Signin.js

---
+++ src/user/Signin.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Layout from "../core/Layout";
+
+const Signin = () => (
+    <Layout title="Sign in" description="Sign in to Node React E-commerce App">
+    </Layout>
+);
+
+export default Signin
