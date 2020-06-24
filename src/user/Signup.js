55e0580847cf354a82d7602a0fdcfd32ca67d9fa
choym0098 <choym0098@gmail.com>
2020-06-24 04:04:24 -0400

    Add files via upload

---
+++ src/user/Signup.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Layout from "../core/Layout";
+
+const Signup = () => (
+    <Layout title="Sign up" description="Sign up to Node React E-commerce App">
+    </Layout>
+);
+
+export default Signup
