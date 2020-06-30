3c1d16f7dd968b9a8f69ecda2ed0288db8cb681d
choym0098 <choym0098@gmail.com>
2020-06-30 12:15:27 -0400

    Add files via upload

--- src/Routes.js
+++ src/Routes.js
@@ -3,16 +3,15 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Signup from './user/Signup';
 import Signin from './user/Signin';
 import Home from './core/Home';
-import Menu from './core/Menu';
 
 const Routes = () => {
     return (
         <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/signin" exact component={Signin}/>
-                <Route path="/signup" exact component={Signup}/>
-            </Switch>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/signin" exact component={Signin}/>
+                    <Route path="/signup" exact component={Signup}/>
+                </Switch>
         </BrowserRouter>
     );
 };
