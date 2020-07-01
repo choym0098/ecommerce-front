5b479e624bd1a5ecd12cbc4f9b621fe08cfcdfa9
choym0098 <choym0098@gmail.com>
2020-07-01 00:26:51 -0400

    Add files via upload

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,9 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../core/Layout";
+import {Redirect} from 'react-router-dom'
+import {signin,  authenticate, isAuthenticated} from "../auth" // no need to write /index
 
-const Signin = () => (
-    <Layout title="Sign in" description="Sign in to Node React E-commerce App">
-    </Layout>
-);
+const Signin = () => {
+    const [values, setValues] = useState({
+        name: '',
+        email: 'choym0098@gmail.com',
+        password: 'qwe123',
+        error: '',
+        loading: false,
+        redirectToReferrer: false,
+    })
+
+    const {email, password, error, loading, redirectToReferrer} = values
+    const {user} = isAuthenticated()
+
+    const handleChange = name => event => {
+        setValues({...values, error: false, [name]: event.target.value })
+    }
+    
+    const clickSubmit = (event) => {
+        event.preventDefault()
+        setValues({...values, error: false, loading: true})
+        signin({email, password}) // same thing as {name: name, email:email, password: password}
+        .then(data => {
+            if(data.error) {
+                setValues({...values, error: data.error, loading: false})
+            } else {
+                authenticate(data, () => {
+                    setValues({
+                        ...values,
+                        loading: false,
+                        redirectToReferrer: true
+                    })
+                })
+            }
+        })
+    }
+
+    const signUpForm = () => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input
+                    onChange={handleChange('email')} 
+                    type="email" 
+                    className="form-control"
+                    value={email}    
+                />
+            </div>
+
+            <div className="form-group">
+                <label className="text-muted">password</label>
+                <input 
+                    onChange={handleChange('password')} 
+                    type="password" 
+                    className="form-control" 
+                    value={password}    
+                />
+            </div>
+            <button onClick={clickSubmit} className="btn btn-primary">Submit</button> 
+        </form>
+    )
+
+    const showError = () => (
+        <div 
+            className="alert alert-danger" 
+            style={{ display: error ? "" : "none" }}
+        >
+            {error}
+        </div>
+    )
+
+    const showLoading = () => (
+        loading && (
+            <div className="alert alert-info">
+                <h2> Loading... </h2>
+            </div>)
+    )
+
+    const redirectUser = () => {
+        if (redirectToReferrer) {
+            if(user && user.role === 1) {
+                return <Redirect to="/admin/dashboard" />
+            } else {
+                return <Redirect to="/user/dashboard" />
+            }
+        }
+    }
+
+    return (
+        <Layout 
+            title="Signin" 
+            description="Sign in to Dashboard"
+            className="container col-md-8 offset-md-2"
+        >
+            {showLoading()}
+            {showError()}
+            {signUpForm()}
+            {redirectUser()}
+        </Layout>
+    )
+}
 
 export default Signin
